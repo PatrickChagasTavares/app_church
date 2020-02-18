d22a8b23c167f632cf0e6fb8dc6bf4ee753af88f
Patrick Chagas <chagas@tradersclub.com.br>
2020-02-18 20:30:58 -0300

    update styled input

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   width: 100%;
-  padding: 0 15px;
+  padding: 0 10px;
   height: 46px;
   background: rgba(0, 0, 0, 0.2);
   border-radius: 4px;
@@ -12,10 +12,10 @@ export const Container = styled.View`
 `;
 
 export const TInput = styled.TextInput.attrs({
-  placeholderTextColor: 'rgba(255,255,255,0.5)',
+  placeholderTextColor: 'rgba(0,0,0,0.5)',
 })`
   flex: 1;
   font-size: 15px;
   margin-left: 10px;
-  color: #fff;
+  color: #000;
 `;
